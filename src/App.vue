b439d5283281242a0eab8325be95c03af215a544
omerfikri <omer.fikri23@gmail.com>
2024-02-08 23:52:58 +0300

    Project created and firebase connected

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <router-view />
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+  .content {
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 20px;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script>
+  import { db } from "../firebase/config.js";
+  export default {};
+</script>
+
+<style scoped></style>
